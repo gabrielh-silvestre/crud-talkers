a9c400cd36c606bb235a7f627307d3c9d3f9535c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-18 20:48:55 -0300

    feat(controller/TalkerController): create method to get talker by id
    - implement new TalkerController method in talker route

--- src/controller/TalkerController.ts
+++ src/controller/TalkerController.ts
@@ -4,15 +4,25 @@ import { TalkerService } from '../services/TalkerService';
 class TalkerController {
   private talkerService: TalkerService;
 
-  constructor( service: TalkerService) {
+  constructor(service: TalkerService) {
     this.talkerService = service;
   }
 
   getAllTalkers = (_req: Request, res: Response) => {
-    const allTalkers = this.talkerService.getAll();
+    const { code, talkers } = this.talkerService.getAll();
 
-    return res.status(200).json(allTalkers);
-  }
+    return res.status(code).json(talkers);
+  };
+
+  getOneTalker = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { code, message, talker } = this.talkerService.getById(Number(id));
+
+    if (message) {
+      return res.status(code).json({ message });
+    }
+    return res.status(code).json(talker);
+  };
 }
 
 export { TalkerController };
--- src/routes/talkerRoute.ts
+++ src/routes/talkerRoute.ts
@@ -4,5 +4,6 @@ import { talkerController } from '../useCases/TalkerUseCase';
 const talkerRoute = express.Router();
 
 talkerRoute.get('/', talkerController.getAllTalkers);
+talkerRoute.get('/:id', talkerController.getOneTalker);
 
 export { talkerRoute };
