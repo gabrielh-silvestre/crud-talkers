e0a29b7ba49552795d9037bec6906b2577e23479
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-18 20:48:06 -0300

    feat(services/TalkerService): create get talker by id service
    - return status code and talker in methods

--- src/services/TalkerService.ts
+++ src/services/TalkerService.ts
@@ -8,7 +8,19 @@ class TalkerService {
   }
 
   getAll() {
-    return this.talkersModel.getAll();
+    const talkers = this.talkersModel.getAll();
+
+    return { code: 200, talkers };
+  }
+
+  getById(id: number) {
+    const talker = this.talkersModel.getById(id);
+
+    if (talker) {
+      return { code: 200, talker };
+    }
+
+    return { code: 404, message: 'Pessoa palestrante não encontrada' };
   }
 }
 
