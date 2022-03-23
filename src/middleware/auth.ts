a961967d817c8c101aca75f46aeb53bc104bfb68
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-23 16:28:27 -0300

    feat(middleware/auth): create middleware for authentication
    - need to refactor to follow MSC arquitecture

---
+++ src/middleware/auth.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { UserModel } from '../model/UserModel';
+
+const userModel = new UserModel();
+
+const auth = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  const findedUser = userModel.findByToken(token);
+
+  if (!findedUser) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+
+  next();
+};
