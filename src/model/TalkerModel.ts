337f17a1663c1c199170e7763ed36ea4048d2a41
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-23 17:10:09 -0300

    feat(model/TalkerModel): implement function to create talker
    - implement stringfy adn write private methods

--- src/model/TalkerModel.ts
+++ src/model/TalkerModel.ts
@@ -21,6 +21,10 @@ class TalkerModel {
     return JSON.parse(strData);
   }
 
+  private stringfy(data: ITalker[]) {
+    return JSON.stringify(data);
+  }
+
   private async read() {
     try {
       this.talkers = this.parse(await fs.readFile('talker.json', 'utf8'));
@@ -30,12 +34,27 @@ class TalkerModel {
     }
   }
 
+  private async write(newContent: string) {
+    try {
+      await fs.writeFile('talker.json', newContent, 'utf8');
+      this.read();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   getAll() {
     return this.talkers;
   }
 
   getById(id: number) {
     return this.talkers.find((t) => t.id === id);
+  }
+
+  create(talker: ITalker) {
+    const talkers = [...this.talkers, talker];
+
+    this.write(this.stringfy(talkers));
   }
 }
 
