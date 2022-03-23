e8d3b58347dda3a773e154311640eb7dd51ad732
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-23 16:08:41 -0300

    feat(services/LoginService): create service for login
    - identify user by email
    - get stored users from login.json

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -28,6 +28,26 @@ class LoginService {
 
     return { code: 201, token: this.loginModel.register(newUser) };
   }
+
+  loginUser(user: { email: string; password: string }) {
+    try {
+      validRegister(user);
+    } catch (err: any) {
+      return { code: 400, message: err.message };
+    }
+
+    const userExist = this.loginModel.findUser(user.email);
+    if (!userExist)
+      return {
+        code: 404,
+        message: `Não existe um usuário com esse email: ${user.email}`,
+      };
+
+    if (userExist.password !== user.password)
+      return { code: 401, message: 'Senha inválida' };
+
+    return { code: 200, token: userExist.token };
+  }
 }
 
 export { LoginService };
