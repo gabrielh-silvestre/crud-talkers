df9fdc2ed3e73d2e4bdc5d40fc184111f83e8002
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-24 00:02:44 -0300

    chore(talkers): remove not used talkers services and old useCase

--- src/modules/talkers/useCases/TalkerUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { TalkerController } from './TalkerController';
-import { TalkerModel } from '../model/TalkerModel';
-import { TalkerService } from '../services/TalkerService';
-
-const talkerModel = new TalkerModel();
-const talkerService = new TalkerService(talkerModel);
-const talkerController = new TalkerController(talkerService);
-
-export { talkerController };
