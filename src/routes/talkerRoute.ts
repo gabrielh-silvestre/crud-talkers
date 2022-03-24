17a0e8e74d399cfddcd2de2aa86416c4ff3f1135
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-03-24 00:01:08 -0300

    refactor(routes/talkerRoute): implement controller of new useCases
    - apply on main code all useCases created

--- src/routes/talkerRoute.ts
+++ src/routes/talkerRoute.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 
-import { talkerController } from '../modules/talkers/useCases/TalkerUseCase';
+import { createTalkerController } from '../modules/talkers/useCases/createTalker';
+import { deleteTalkerController } from '../modules/talkers/useCases/deleteTalker';
+import { findAllTalkerController } from '../modules/talkers/useCases/findAllTalkers';
+import { findTalkerByIdController } from '../modules/talkers/useCases/findTalkerById';
+import { searchByTalkerNameController } from '../modules/talkers/useCases/searchByTalkerName';
+import { updateTalkerController } from '../modules/talkers/useCases/updateTalker';
+
 import { loginController } from '../modules/users/useCases/LoginUseCase';
 
 const talkerRoute = express.Router();
@@ -8,29 +14,29 @@ const talkerRoute = express.Router();
 talkerRoute.get(
   '/search',
   loginController.authUserByToken,
-  talkerController.searchByName
+  searchByTalkerNameController.handle
 );
 
 talkerRoute.post(
   '/',
   loginController.authUserByToken,
-  talkerController.createTalker
+  createTalkerController.handle
 );
 
 talkerRoute.put(
   '/:id',
   loginController.authUserByToken,
-  talkerController.updateTalker
+  updateTalkerController.handle
 );
 
 talkerRoute.delete(
   '/:id',
   loginController.authUserByToken,
-  talkerController.deleteTalker
+  deleteTalkerController.handle
 );
 
-talkerRoute.get('/', talkerController.getAllTalkers);
+talkerRoute.get('/', findAllTalkerController.handle);
 
-talkerRoute.get('/:id', talkerController.getOneTalker);
+talkerRoute.get('/:id', findTalkerByIdController.handle);
 
 export { talkerRoute };
